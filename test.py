17144eff37f788f5af8763026b34c0d83c3e8f05
Christopher van den Berg <cvandenberg1105@googlemail.com>
2021-02-09 20:47:27 +0000

    Removed some code

--- test.py
+++ test.py
@@ -1,8 +1,5 @@
-import math
-import os
-import sys
-
 import requests
 
-name = input("Your name? ")
-print("Hello,", name)
+r = requests.get("https://coreyms.com")
+print(r.status_code)
+print(r.ok)
